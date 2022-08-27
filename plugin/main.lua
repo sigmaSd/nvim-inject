15e41c10ff8730c3297329f8426c7d97efd48c97
Nbiba Bedis <bedisnbiba@gmail.com>
2022-08-27 17:26:23 +0100

    Add lua support

--- plugin/main.lua
+++ plugin/main.lua
@@ -10,16 +10,16 @@ local function inject()
     if lang == "typescript" then
         local all_langs = { 'rust', 'lua', 'python' }
         for _, known_lang in ipairs(all_langs) do
-            query = query .. string.format([[
+            query = query .. string.format(--[[query]] [[
 (
-    (comment) @_comment (#eq? @_comment "/*%s*/")
+    (comment) @_comment (#contains? @_comment "%s")
     (
         (template_string) @%s
         (#offset! @%s 0 1 0 -1)
     )
 )
 (
-    (comment) @_comment (#eq? @_comment "/*%s*/")
+    (comment) @_comment (#contains? @_comment "%s")
     (expression_statement
         (template_string) @%s
         (#offset! @%s 0 1 0 -1)
@@ -31,22 +31,45 @@ local function inject()
     elseif lang == "rust" then
         local all_langs = { 'typescript', 'lua', 'python' }
         for _, known_lang in ipairs(all_langs) do
-            query = query .. string.format([[
+            query = query .. string.format(--[[query]] [[
 (
-    (block_comment) @_comment (#eq? @_comment "/*%s*/")
+    (block_comment) @_comment (#contains? @_comment "%s")
     (
         (raw_string_literal) @%s
         (#offset! @%s 0 3 0 -3)
     )
 )
 (
-    (block_comment) @_comment (#eq? @_comment "/*%s*/")
+    (block_comment) @_comment (#contains? @_comment "%s")
     (expression_statement
         (raw_string_literal) @%s
         (#offset! @%s 0 3 0 -3)
     )
 )
 ]]           , known_lang, known_lang, known_lang, known_lang, known_lang, known_lang)
+        end
+    elseif lang == "lua" then
+        -- comment [0, 10] - [0, 22]
+        -- expression_list [0, 22] - [0, 33]
+        --   value: string [0, 22] - [0, 33]
+        local all_langs = { 'rust', 'typescript', 'query' }
+        for _, known_lang in ipairs(all_langs) do
+            query = query .. string.format(--[[query]] [[
+(
+    (comment) @_comment (#contains? @_comment "%s")
+    (string) @%s
+    (#lua-match? @%s "^%%[%%[")
+    (#offset! @%s 0 2 0 -2)
+)
+(
+    (comment) @_comment (#contains? @_comment "%s")
+    (expression_list
+        (string) @%s
+        (#lua-match? @%s "^%%[%%[")
+        (#offset! @%s 0 2 0 -2)
+    )
+)
+]]           , known_lang, known_lang, known_lang, known_lang, known_lang, known_lang, known_lang, known_lang, known_lang)
         end
     else
         print("Please PR this language!")
